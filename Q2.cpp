1d5251b5949d30cfa29e2015af1a4916ddb83bbf
mems250005023 <mems250005023@iiti.ac.in>
2025-10-17 16:29:18 +0530

    Add Color struct and modify RGB values
    Initialize an array of Color structs and invert RGB values for the first three colors. Print the modified color values.

--- Q2.cpp
+++ Q2.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
+
+struct Color {
+    int red;
+    int green;
+    int blue;
+};
+
+int main() {
+    Color colors[5] = {
+        {10, 15, 20},
+        {5, 7, 12},
+        {90, 80, 5000},
+        {25, 55, 22},
+        {23, 34, 4}
+    };
+
+    for (int i = 0; i < 3; ++i) {
+        colors[i].red = 255 - colors[i].red;
+        colors[i].green = 255 - colors[i].green;
+        colors[i].blue = 255 - colors[i].blue;
+    }
+
+        cout << "Color " << i + 1 << ": "
+             << "Red = " << colors[i].red << ", "
+             << "Green = " << colors[i].green <<", "
+             << "Blue = "<< colors[i].blue << endl;
+    }
+    return 0;
+}
+
+
 
