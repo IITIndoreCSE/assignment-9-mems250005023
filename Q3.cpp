77e349d977b3a392499c5696931e06b2c9404d2b
mems250005023 <mems250005023@iiti.ac.in>
2025-10-17 16:40:04 +0530

    Implement point input and count first quadrant points

--- Q3.cpp
+++ Q3.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
+
+struct Point {
+    float x;
+    float y;
+};
+
+int main() {
+    Point points[7];
+
+    for (int i = 0; i < 7; ++i) {
+        cout << "Enter x and y for point " << i + 1 << ": ";
+        cin >> points[i].x >> points[i].y;
+    }
+    int c= 0;
+    for (int i = 0; i < 7; ++i) {
+        if (points[i].x > 0 && points[i].y > 0) {
+            ++c;
+        }
+    }
+    cout << "Number of points in the first quadrant: " << c << endl;
+
+    return 0;
+}
+
 
