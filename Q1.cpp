def1d5c052a8a1d635e54d189f76d53b94a82d04
mems250005023 <mems250005023@iiti.ac.in>
2025-10-17 16:13:10 +0530

    Add item structure and filter by cost
    Fixed array initialization and loop condition to iterate through all items.

--- Q1.cpp
+++ Q1.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using namespace std;
+
+struct Item {
+    int id;
+    float cost;
+};
+
+int main() {
+    Item items[6] = {
+        {1, 98.50},
+        {2, 13.00},
+        {3, 50.25},
+        {4, 3.00}
+    };
+
+    cout << "Items with cost greater than 50.00:" << endl;
+    for (int i = 0; i < 6; i++) {
+        if (items[i].cost > 50.00) {
+            cout << "Item ID: " << items[i].id << ", Cost: " << items[i].cost << endl;
+        }
+    }
+
+    return 0;
+}
+
+	
+
+
 
